62404a8f54481864bc2a8a100b3515d6dea39391
SSI07 <sheikismail884@gmail.com>
2020-11-08 13:54:36 +0530

    new java file

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,22 @@
+
+import java.util.Random;
+
+import static java.lang.Math.abs;
+
 public class Hello {
-    public static void main(String args[]){
-        System.out.println("Hello World!!!!");
-    }
+	
+	public static void main(String[] args) {
+		Random rand=new Random();
+		byte val1= (byte) rand.nextInt(Byte.MAX_VALUE);
+		short val2= (short) rand.nextInt(Short.MAX_VALUE);
+		int val3= rand.nextInt(Integer.MAX_VALUE);
+		long total=50000+10*(val1+val2+val3);
+
+		int pound=200;
+		double kilogram=pound*(0.45359237);
+
+		System.out.println("Total: "+total +"\n"+pound+" pound= "+kilogram+" Kilogram");
+
+	}
+
 }
