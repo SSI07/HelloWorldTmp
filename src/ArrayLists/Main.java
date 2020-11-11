7a05f2dcb4f1bafbd4a437668ed6faafcc83b358
Sheik2252 <ssirfan1999@gmail.com>
2020-11-11 13:20:36 +0530

    arraylist add,edit,find.

--- src/ArrayLists/Main.java
+++ src/ArrayLists/Main.java
@@ -5,14 +5,64 @@ import java.util.Scanner;
 public class Main {
     private static Scanner sc=new Scanner(System.in);
     public static void main(String args[]){
-        System.out.println("Enter name");
-        String name=sc.nextLine();
-        System.out.println("Enter Phone number");
-        long phone=sc.nextLong();
-        //sc.nextLine();
         MobilePhone ph=new MobilePhone();
-        ph.addContacts(new Contacts(name,phone));
-        ph.list();
+
+        choice();
+        switch_statement(ph);
+        //sc.nextLine();
+
         sc.close();
+    }
+
+
+    public static void choice(){
+        System.out.println(
+                "0 to display choice list \n"+
+                "1 to add list \n"+
+                "2 to display list \n"+
+                        "3 to search contact using name and edit contact \n"+
+                "4 to quit \n"+"Enter your choice :\n" );
+
+    }
+    public static void switch_statement(MobilePhone ph){
+        boolean quit=false;
+        while(!quit) {
+
+            int choice=sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 0:
+                    choice();
+                    break;
+
+                case 1:
+                    System.out.println("Enter name");
+                    String name = sc.nextLine();
+                    System.out.println("Enter Phone number");
+                    long phone = sc.nextLong();
+                    ph.addContacts(new Contacts(name, phone));
+                    break;
+
+                case 2:
+                    ph.list();
+                    break;
+
+                case 3:
+                    System.out.println("Enter name");
+                    String name2 = sc.nextLine();
+                    System.out.println("Enter new Phone number");
+                    long phone2 = sc.nextLong();
+                    ph.update_contact(new Contacts(name2, phone2));
+                    break;
+
+                case 4:
+                    quit=true;
+                    break;
+
+                default:
+                    choice();
+                    break;
+            }
+        }
     }
 }
--- src/ArrayLists/MobilePhone.java
+++ src/ArrayLists/MobilePhone.java
@@ -8,11 +8,12 @@ public class MobilePhone {
 
 
     public void addContacts(Contacts contact){
-        if(contactsArrayList.contains(contact.getName())){
+        if(finding_existing_users(contact)){
             System.out.println("User already exists");
         }
         else {
             contactsArrayList.add(contact);
+            System.out.println("Name and Contact added Successfully.");
         }
     }
 
@@ -29,8 +30,37 @@ public class MobilePhone {
     public void list(){
         for(int i=0;i<contactsArrayList.size();i++){
             Contacts c=contactsArrayList.get(i);
-            System.out.println(c.getName()+" - "+c.getPhone());
+            System.out.println((i+1)+". "+c.getName()+" - "+c.getPhone());
            // System.out.println(contactsArrayList.get(i).getName());
         }
+    }
+    public void update_contact(Contacts c){
+        int index=finding_index(c.getName());
+        if(index!=-1){
+            contactsArrayList.set(index,c);
+            System.out.println("Contact Updated Successfully");
+        }
+        else{
+            System.out.println("Name not found");
+        }
+    }
+    public boolean finding_existing_users(Contacts contact){
+        for(int i=0;i<contactsArrayList.size();i++){
+            Contacts c=contactsArrayList.get(i);
+            if(c.getName().equals(contact.getName()))
+                return true;
+            // System.out.println(contactsArrayList.get(i).getName());
+        }
+        return false;
+    }
+
+    public int finding_index(String name){
+        for(int i=0;i<contactsArrayList.size();i++){
+            Contacts c=contactsArrayList.get(i);
+            if(c.getName().equals(name))
+                return i;
+            // System.out.println(contactsArrayList.get(i).getName());
+        }
+        return -1;
     }
 }
